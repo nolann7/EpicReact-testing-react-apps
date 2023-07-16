9ab824585bd8071c09069a93ba54ab6f526d8a85
nolann7 <nolann709@gmail.com>
2023-07-16 21:28:44 +0300

    1 exercise

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -10,24 +10,32 @@ import Counter from '../../components/counter'
 // https://react.dev/blog/2022/03/08/react-18-upgrade-guide#configuring-your-testing-environment
 // Luckily, it's handled for you by React Testing Library :)
 global.IS_REACT_ACT_ENVIRONMENT = true
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
 
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
   //
+  const div = document.createElement('div')
   // 🐨 append the div to document.body (💰 document.body.append)
   //
+  document.body.append(div)
   // 🐨 use createRoot to render the <Counter /> to the div
+  const root = createRoot(div)
+  act(() => root.render(<Counter />))
   // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
+  const [decrement, increment] = div.querySelectorAll('button')
   // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
+  const message = div.firstChild.querySelector('div')
   //
   //
   // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 act(() => increment.click()))
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 act(() => decrement.click()))
-  // 🐨 assert the message.textContent
+  expect(message.textContent).toBe('Current count: 0')
+  act(() => increment.click())
+  expect(message.textContent).toBe('Current count: 1')
+  act(() => decrement.click())
+  expect(message.textContent).toBe('Current count: 0')
   //
   // 🐨 cleanup by removing the div from the page (💰 div.remove())
   // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
