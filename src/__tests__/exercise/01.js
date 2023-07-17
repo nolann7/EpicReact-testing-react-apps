76503653403620b1d4e0bf8d8fb00b63e6fc9c68
nolann7 <nolann709@gmail.com>
2023-07-17 11:54:06 +0300

    exercise 1: extra 1

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -15,30 +15,27 @@ beforeEach(() => {
 })
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
   const div = document.createElement('div')
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
   document.body.append(div)
-  // 🐨 use createRoot to render the <Counter /> to the div
   const root = createRoot(div)
   act(() => root.render(<Counter />))
-  // 🐨 get a reference to the increment and decrement buttons:
-  const [decrement, increment] = div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  const message = div.firstChild.querySelector('div')
-  //
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  expect(message.textContent).toBe('Current count: 0')
-  act(() => increment.click())
-  expect(message.textContent).toBe('Current count: 1')
-  act(() => decrement.click())
-  expect(message.textContent).toBe('Current count: 0')
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
-})
 
-/* eslint no-unused-vars:0 */
+  const [decrement, increment] = div.querySelectorAll('button')
+  const message = div.firstChild.querySelector('div')
+
+  expect(message.textContent).toBe('Current count: 0')
+  const incrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  act(() => increment.dispatchEvent(incrementClickEvent))
+  expect(message.textContent).toBe('Current count: 1')
+  const decrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  act(() => decrement.dispatchEvent(decrementClickEvent))
+  expect(message.textContent).toBe('Current count: 0')
+})
