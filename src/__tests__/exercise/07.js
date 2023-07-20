238f4b1f7bbebe336df75ecf4f1f1d1e9e70d835
nolann7 <nolann709@gmail.com>
2023-07-20 19:43:12 +0300

    7 exercise: my test that clicking on toggle theme button works

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -3,7 +3,10 @@
 
 import * as React from 'react'
 import {render, screen} from 'test/test-utils'
+import userEvent from '@testing-library/user-event'
+
 import EasyButton from '../../components/easy-button'
+import App from 'examples/easy-button'
 
 test('renders with the light styles for the light theme', () => {
   render(<EasyButton>Easy</EasyButton>)
@@ -21,4 +24,20 @@ test('renders with the dark styles for the dark theme', () => {
     background-color: black;
     color: white;
   `)
+})
+
+test('theme toggles from light to dark and back when button toggle is clicked', async () => {
+  render(<App />)
+  const user = userEvent.setup()
+  const easyButton = screen.getByRole('button', {name: /Easy/i})
+  const toggleButton = screen.getByRole('button', {name: /Toggle theme/i})
+
+  // from default light to dark
+  await user.click(toggleButton)
+  expect(easyButton).toHaveStyle('color: white; background-color: black;')
+  expect(toggleButton).toHaveTextContent(/dark/i)
+  // from dark theme to light
+  await user.click(toggleButton)
+  expect(easyButton).toHaveStyle('color: black; background-color: white;')
+  expect(toggleButton).toHaveTextContent(/light/i)
 })
