add97be2042e77b3b8f6362a8df5c7fb23996a71
nolann7 <nolann709@gmail.com>
2023-07-20 19:07:33 +0300

    exercise 7: extra 3

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,16 +2,8 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render as rtlRender, screen} from '@testing-library/react'
-import {ThemeProvider} from '../../components/theme'
+import {render, screen} from 'test/test-utils'
 import EasyButton from '../../components/easy-button'
-
-function render(ui, {theme = 'light', ...options} = {}) {
-  function AllProviders({children}) {
-    return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
-  }
-  return rtlRender(ui, {wrapper: AllProviders, ...options})
-}
 
 test('renders with the light styles for the light theme', () => {
   render(<EasyButton>Easy</EasyButton>)
@@ -21,6 +13,7 @@ test('renders with the light styles for the light theme', () => {
     color: black;
   `)
 })
+
 test('renders with the dark styles for the dark theme', () => {
   render(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
   const button = screen.getByRole('button', {name: /easy/i})
